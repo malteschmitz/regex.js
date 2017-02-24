75ba81b55a54fc3606862037a20c88b6c1d352cd
Sebastian Hungerecker <sebastian.hungerecker@isp.uni-luebeck.de>
2017-02-24 20:59:11 +0100

    Use node typings instead of "declare var:any"

--- main.ts
+++ main.ts
@@ -4,8 +4,8 @@ import { printer } from "./printer"
 import { parser } from "./regex-parser";
 import { regex2nfa } from "./regex2nfa";
 import { Nfa } from "./nfa";
+import * as process from "process";
 
-declare var process: any;
 var regex: string = process.argv[2];
 var word: string = process.argv[3];
 if (!regex || !word) {
